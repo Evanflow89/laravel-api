81b6268a28cb0015aea2eced372266bb7ca4240c
Evanflow89 <ivanbaragone@gmail.com>
2022-07-26 20:27:21 +0200

    V for Vue

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <h1>HELLO AGAIN VUE!</h1>
+    <h1>Post List</h1>
+    <ul>
+      <li v-for="post in posts" key="post.slug">
+        {{ post.title }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:8000/api/posts")
+      .then((response) => {
+        this.posts = response.data;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  },
 };
 </script>
 
